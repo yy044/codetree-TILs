e393c9f03e323fd57f6d51bc9d3cf80366952fff
yy044 <115568749+yy044@users.noreply.github.com>
2024-03-31 14:44:50 +0900

    Title: 변수 값 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240331/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1, b = 2, c = 3;
+    a = c;
+    b = c;
+    cout << a << " " << b << " " << c;
+    return 0;
+}
